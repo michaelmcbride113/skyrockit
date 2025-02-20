523cee2b6acf466c92d2e1409997240ac47060c9
Michael McBride <michaelmcbride113@gmail.com>
2025-02-20 14:38:27 -0500

    Added nav, added application tracking

--- controllers/applications.js
+++ controllers/applications.js
@@ -9,14 +9,49 @@ const User = require('../models/user.js');
 // GET /users/:userID/applications - 
 // we use router. instead of app. cause we're in the applications.js page
 // try/catch is a rudementary way to handle errors
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        res.render('applications/index.ejs')
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Render index.ejs, passing in all of the current user's
+      // applications as data in the context object.
+      res.render('applications/index.ejs', {
+        applications: currentUser.applications,
+      });
     } catch (error) {
-        console.log(error);
-        res.redirect('/');
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/');
     }
-});
+  });
+  
+
+// GET /users/:userId/applications/new
+router.get('/new', async (req, res) => {
+    res.render('applications/new.ejs')
+})
+
+// POST /users/:userId/applications
+// controllers/applications.js`
+
+router.post('/', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Push req.body (the new form data object) to the
+      // applications array of the current user
+      currentUser.applications.push(req.body);
+      // Save changes to the user
+      await currentUser.save();
+      // Redirect back to the applications index view
+      res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+  
 
 
 
